d21aa1f5eb9b26320498ee2b5ee78c6bf5e9a73d
Aston Nelson <jerrel004@gmai.com>
2020-04-01 01:20:05 -0500

    t

--- day2/Calculator.py
+++ day2/Calculator.py
@@ -1,1 +1,20 @@
-print('Aston')
+First_num = input('Enter first number:\n')
+symbol = input('Enter Operation (+, -, *, /):\n')
+Second_num = input('Enter Second Number:\n')
+
+First_num = float(First_num)
+Second_num = float(Second_num)
+
+out = None
+
+if symbol == '+':
+    out = First_num + Second_num
+elif symbol == '-':
+    out = First_num - Second_num
+elif symbol == '*':
+    out = First_num *Second_num
+elif symbol == '/':
+    out = First_num / Second_num
+
+print('answer: ' + str(out))
+
